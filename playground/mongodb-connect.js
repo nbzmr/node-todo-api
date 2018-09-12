1d1bddc694d8b808e06ed2c3cfc94a2ca92f87b7
unknown <jklfaeifncaslkfj>
2018-09-12 03:22:46 -0700

    a

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
+
+const id = new ObjectID()
+console.log(id)
+
+MongoClient.connect('mongodb://localhost:27017/todoApp', (err, client) => {
+    if(err) {
+        console.log('unable to connect to database server')
+        return null
+    }
+    console.log('connect to database server succesfully')
+
+    const db = client.db('todoApp')
+
+    db.collection('todos').insertOne({
+        text: 'somthing todo',
+        completed: false
+    }, (err, result) => {
+        if(err) {
+            console.log('unable to insert data into the database', err)
+            return
+        }
+        console.log(JSON.stringify(result.ops, null, 2))
+    })
+
+    db.collection('users').insertOne({
+        name: 'mike',
+        age: 27,
+        location: 'somewhere'
+    }, (err, result) => {
+        if(err) {
+            console.log('unable to insert data', err)
+            return null
+        }
+        console.log(JSON.stringify(result.ops, null, 2))
+        console.log(result.ops[0]._id.getTimestamp())
+    })
+
+    client.close()
+})
---
+++ playground/mongodb-find.js
@@ -0,0 +1,47 @@
+const mongodb = require('mongodb')
+const {MongoClient, ObjectID} = mongodb
+
+MongoClient.connect('mongodb://localhost:27017/todoApp', (err, client) => {
+    if(err) {
+        console.log('unable to connect to database server', err)
+        return null
+    }
+    console.log('connected to server successfully')
+
+    const db = client.db('todoApp')
+
+    // db.collection('todos').find({
+    //     completed: false,
+    //     _id: new ObjectID('5b97e4a2d007f04d9860bbde')
+    // }).toArray().then((docs) => {
+    //     console.log(JSON.stringify(docs, null, 2))
+    // }, (err) => {
+    //     console.log('unable to fetch the data', err)
+    // })
+
+    // db.collection('users')
+    // .find({
+    //     name: 'mike',
+    //     _id: new ObjectID('5b97e4a2d007f04d9860bbdf')
+    // })
+    // .toArray()
+    // .then((docs) => {
+    //     console.log(JSON.stringify(docs, null, 2))
+    // }, (err) => {
+    //     console.log('unable to find the data', err)
+    // })
+
+    db.collection('users')
+    .find({
+        _id: new ObjectID('5b98d14f27230fd56d7cb5bc'),
+        name: 'mike'
+    })
+    .count()
+    .then((count) => {
+        console.log(count)
+    },(err) => {
+        console.log(err)
+    })
+
+    client.close()
+})
