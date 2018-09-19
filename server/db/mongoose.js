db084914d0e7c462456a88d3f5cdba5be808bdf8
unknown <jklfaeifncaslkfj>
2018-09-19 06:22:50 -0700

    fixed mongodb server url for mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todoApp', {useNewUrlParser: true})
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds131151.mlab.com:31151/todos', {useNewUrlParser: true} || 'mongodb://localhost:27017/todoApp', {useNewUrlParser: true})
 
 module.exports = {
     mongoose
