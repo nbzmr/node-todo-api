b2d0eada85d69a7b634a145c66dea1c2d17f855b
unknown <jklfaeifncaslkfj>
2018-09-19 06:26:07 -0700

    a

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds131151.mlab.com:31151/todos', {useNewUrlParser: true} || 'mongodb://localhost:27017/todoApp', {useNewUrlParser: true})
-
+// mongoose.connect('mongodb://localhost:27017/todoApp', {useNewUrlParser: true})
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds131151.mlab.com:31151/todos', {useNewUrlParser: true})
 module.exports = {
     mongoose
 }
